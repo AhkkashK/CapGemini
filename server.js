5caadb6b8c2647350eadb4f310a846986b50fcd3
Lin Emmanuel <emmanuellin135@hotmail.com>
2024-11-20 10:40:50 +0100

    modif server.js log file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { Pool } = require('pg');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,17 +14,43 @@ const pool = new Pool({
   port: 5432,
 });
 
+// Fonction pour ajouter des logs dans un fichier
+const logToFile = (message) => {
+  const timestamp = new Date().toLocaleString();
+  const logMessage = `[${timestamp}] ${message}\n`;
+  fs.appendFile('server.log', logMessage, (err) => {
+    if (err) {
+      console.error('Erreur lors de l\'enregistrement du log:', err);
+    }
+  });
+};
+
 // Vérifier la connexion à la base de données
 pool.connect()
-  .then(() => console.log('Connecté à PostgreSQL'))
-  .catch(err => console.error('Erreur de connexion à PostgreSQL:', err));
+  .then(() => {
+    console.log('Connecté à PostgreSQL');
+    logToFile('Connecté à PostgreSQL');
+  })
+  .catch(err => {
+    console.error('Erreur de connexion à PostgreSQL:', err);
+    logToFile(`Erreur de connexion à PostgreSQL: ${err.message}`);
+  });
 
 // Middleware pour parser le JSON
 app.use(express.json());
 
+// Middleware pour logguer les requêtes entrantes
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    const logMessage = `[${new Date().toLocaleTimeString()}] ${req.method} ${req.originalUrl} - Code de statut: ${res.statusCode}`;
+    logToFile(logMessage);
+  });
+  next();
+});
+
 // Endpoint de base
 app.get('/', (req, res) => {
-  res.send('Hello, world! Welcome to your PostgreSQL API!');
+  res.send('Etablissement API');
 });
 
 /* READ */
@@ -33,14 +60,14 @@ app.get('/etablissements', async (req, res) => {
     res.json(result.rows);
   } catch (error) {
     console.error('Erreur lors de la lecture des données:', error);
+    logToFile(`Erreur lors de la lecture des données: ${error.message}`);
     res.status(500).send('Erreur lors de la lecture des données');
   }
 });
 
 /*READ2*/
-
 app.get('/etablissements/:siret', async (req, res) => {
-  const { siret } = req.params; // Récupérer le SIRET depuis les paramètres de l'URL
+  const { siret } = req.params;
 
   try {
     const result = await pool.query('SELECT * FROM etablissements WHERE siret = $1', [siret]);
@@ -49,24 +76,22 @@ app.get('/etablissements/:siret', async (req, res) => {
       return res.status(404).send('Établissement non trouvé');
     }
 
-    res.json(result.rows[0]); // Renvoyer uniquement le premier résultat trouvé
+    res.json(result.rows[0]);
   } catch (error) {
     console.error('Erreur lors de la lecture des données:', error);
+    logToFile(`Erreur lors de la lecture des données: ${error.message}`);
     res.status(500).send('Erreur lors de la lecture des données');
   }
 });
-
 
 /* CREATE */
 app.post('/etablissements', async (req, res) => {
   const siret = req.body.siret;
 
   try {
-    // Vérifier si le numéro de SIRET existe déjà dans la base de données
     const existingEntry = await pool.query('SELECT * FROM etablissements WHERE siret = $1', [siret]);
 
     if (existingEntry.rows.length > 0) {
-      // Si le SIRET existe déjà, renvoyer un message d'erreur
       return res.status(400).send('Le numéro de SIRET existe déjà dans la base de données.');
     }
 
@@ -154,40 +179,36 @@ app.post('/etablissements', async (req, res) => {
     await pool.query(query, values);
     res.status(201).send('Enregistrement ajouté avec succès');
   } catch (error) {
-    console.error('Erreur lors de l\'ajout des données :', error);
-    res.status(500).send('Erreur lors de l\'ajout des données');
+    console.error("Erreur lors de l'ajout des données :", error);
+    logToFile(`Erreur lors de l'ajout des données : ${error.message}`);
+    res.status(500).send("Erreur lors de l'ajout des données");
   }
 });
 
 /* UPDATE */
 app.put('/etablissements/:siret', async (req, res) => {
-  const { siret } = req.params; // Le numéro de SIRET de l'établissement à mettre à jour
-  const updatedFields = req.body; // Champs à mettre à jour (tous les champs fournis dans le body)
+  const { siret } = req.params;
+  const updatedFields = req.body;
 
-  // Vérifier que le champ 'siret' n'est pas inclus dans le body
   if (updatedFields.siret) {
     return res.status(400).send('Le champ "siret" ne peut pas être modifié');
   }
 
-  // Vérifier que le corps de la requête n'est pas vide
   if (Object.keys(updatedFields).length === 0) {
     return res.status(400).send('Aucun champ à mettre à jour');
   }
 
   try {
-    // Vérifier si le SIRET existe déjà dans la base de données
     const checkSiretResult = await pool.query('SELECT * FROM etablissements WHERE siret = $1', [siret]);
 
     if (checkSiretResult.rows.length === 0) {
       return res.status(404).send('Établissement non trouvé');
     }
 
-    // Construire la requête SQL de mise à jour de manière dynamique
     let updateQuery = 'UPDATE etablissements SET ';
     let updateValues = [];
     let counter = 1;
 
-    // Parcourir les champs envoyés dans le corps de la requête et ajouter les champs à la requête SQL
     for (let field in updatedFields) {
       if (updatedFields.hasOwnProperty(field)) {
         updateQuery += `${field} = $${counter}, `;
@@ -196,26 +217,23 @@ app.put('/etablissements/:siret', async (req, res) => {
       }
     }
 
-    // Retirer la dernière virgule de la requête SQL
     updateQuery = updateQuery.slice(0, -2);
     updateQuery += ` WHERE siret = $${counter}`;
     updateValues.push(siret);
 
-    // Exécuter la requête de mise à jour
     const updateResult = await pool.query(updateQuery, updateValues);
 
-    // Vérifier si la mise à jour a eu un effet
     if (updateResult.rowCount === 0) {
       return res.status(404).send('Établissement non trouvé');
     }
 
     res.send('Établissement mis à jour avec succès');
   } catch (error) {
-    console.error('Erreur lors de la mise à jour de l\'établissement:', error);
+    console.error("Erreur lors de la mise à jour de l'établissement:", error);
+    logToFile(`Erreur lors de la mise à jour de l'établissement: ${error.message}`);
     res.status(500).send('Erreur serveur');
   }
 });
-
 
 /* DELETE */
 app.delete('/etablissements/:siret', async (req, res) => {
@@ -228,6 +246,7 @@ app.delete('/etablissements/:siret', async (req, res) => {
     res.send('Enregistrement supprimé');
   } catch (error) {
     console.error('Erreur lors de la suppression:', error);
+    logToFile(`Erreur lors de la suppression: ${error.message}`);
     res.status(500).send('Erreur lors de la suppression');
   }
 });
@@ -235,4 +254,5 @@ app.delete('/etablissements/:siret', async (req, res) => {
 // Lancer le serveur
 app.listen(PORT, () => {
   console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
+  logToFile(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
 });
